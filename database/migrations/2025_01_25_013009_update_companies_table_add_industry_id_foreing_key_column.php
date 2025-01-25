34865d6250ab16f9363b8aa7f57d4e15519ed840
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-25 11:49:50 +0100

    feat(migration): add foreing key column to companies and employees table

---
+++ database/migrations/2025_01_25_013009_update_companies_table_add_industry_id_foreing_key_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->unsignedBigInteger('industry_id')->after('id');
+            $table->foreign('industry_id')->references('id')->on('industries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropForeign('companies_industry_id_foreign');
+            $table->dropColumn('industry_id');
+        });
+    }
+};
