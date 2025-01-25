ae7c3ff63614fce932621a7fa144d8ac3f92ec94
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-25 12:15:37 +0100

    feat: add belongsTo relationship in employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
     use HasFactory;
+    /**
+     * Get the company that owns the EmployeeSeeder
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
