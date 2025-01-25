7d8a54c7a4b008aac4026b7034f9aea68fe19310
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-25 11:44:13 +0100

    feat(migration): add columns to employees table: name, lastname, phone, email, job_title

--- database/migrations/2025_01_24_235626_create_employees_table.php
+++ database/migrations/2025_01_24_235626_create_employees_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->integer('phone')->unique();
+            $table->string('email')->unique();
+            $table->string('job_title');
             $table->timestamps();
         });
     }
