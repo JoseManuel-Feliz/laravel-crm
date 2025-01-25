972f23cb6fd45d0b0c39ab2c625506f79e34ff6f
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-25 12:16:29 +0100

    feat: add belongsTo and belongsToMany relationships in company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Company extends Model
 {
     use HasFactory;
+
+    /**
+     * The Employees tha belong to the CompanySeeder
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function employees(): BelongsToMany
+    {
+        return $this->belongsToMany(Employee::class);
+    }
+
+    /**
+     * Get the industries that owns the Industry
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function industries(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
