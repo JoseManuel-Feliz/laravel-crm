bccb57caf1804b86719d99f61bcca2b7ca9f7e21
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-25 11:45:47 +0100

    feat(migration): add columns to companies table: name, vat_num, phone, email, logo, city, region

--- database/migrations/2025_01_24_232458_create_companies_table.php
+++ database/migrations/2025_01_24_232458_create_companies_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('vat_num')->unique();
+            $table->integer('phone')->unique();
+            $table->string('email')->unique();
+            $table->text('logo');
+            $table->string('city');
+            $table->string('region');
             $table->timestamps();
         });
     }
